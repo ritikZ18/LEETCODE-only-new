7ffa3a19070c3741735e6dddf8946734d7b6a53a
Ritik Zambre <116812243+ritikZ18@users.noreply.github.com>
2025-03-07 22:52:44 -0700

    File updated at 3/7/2025, 10:52:46 PM

--- 2273-FindResultantArrayAfterRemovingAnagrams/2273-FindResultantArrayAfterRemovingAnagrams.py
+++ 2273-FindResultantArrayAfterRemovingAnagrams/2273-FindResultantArrayAfterRemovingAnagrams.py
@@ -4,13 +4,26 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[str]
         """
-        result=[]
-        prev=None
-        for i in words:
-            sort=''.join(sorted(i))
-            if prev!=sort:
-                result.append(i)
-                prev=sort
-        return result
+        group = defaultdict(list)
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        result = []
+        prev_key = None  #tracks prev sstored string
 
+        for s in words : 
+            count = [0] * 26
 
+            for char in s : 
+                #get the index of the matched letter
+                index = letters.index(char)
+                count[index] += 1
+            key = tuple(count)       # the char  with all 0 , 1  , create tupled key
+           
+
+        
+            #  only add if not consective anagram 
+            if key != prev_key : 
+                result.append(s)
+        
+            prev_key = key      # update for prev_key with key, for iteration
+
+        return result 
