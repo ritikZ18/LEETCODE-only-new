55e9fa4257a307513980d8a77b6c9475b7091bc1
Ritik Zambre <116812243+ritikZ18@users.noreply.github.com>
2025-03-18 23:32:04 -0700

    File updated at 3/18/2025, 11:32:04 PM

--- 442-FindAllDuplicatesinanArray/442-FindAllDuplicatesinanArray.py
+++ 442-FindAllDuplicatesinanArray/442-FindAllDuplicatesinanArray.py
@@ -4,12 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        result = []
-        #  logic is simple the visited are marked as negativee
-        for num in nums : 
-                num = abs(num)
-                if nums[num-1] < 0 : 
-                    result.append(num)
-                else : 
-                    nums[num-1] *= -1
-        return result 
+        hashmap=set()
+        duplicte=[]
+        for ele in nums:
+            if ele in hashmap:
+                duplicte.append(ele)
+                continue
+            hashmap.add(ele)
+        return duplicte
+
+        
