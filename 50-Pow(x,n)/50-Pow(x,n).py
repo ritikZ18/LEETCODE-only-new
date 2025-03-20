d6158b4ad4e04d64ab74ca1cc8587291ba4e8a32
Ritik Zambre <116812243+ritikZ18@users.noreply.github.com>
2025-03-19 20:47:17 -0700

    File updated at 3/19/2025, 8:47:16 PM

--- 50-Pow(x,n)/50-Pow(x,n).py
+++ 50-Pow(x,n)/50-Pow(x,n).py
@@ -1,22 +1,17 @@
 class Solution(object):
     def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
-
+        if n == 0:
+            return 1
         
-        if n == 0 : 
-            return 1 
+        if n < 0:
+            return 1 / self.myPow(x, -n)
         
-        if n < 0 : 
-            return 1 / self.myPow(x,-n)
-
-        half = self.myPow(x, n//2)
-
-        return half * half if n % 2 == 0  else x * half * half
-
-                
-
-            
+        result = 1
+        while n > 0:
+            if n % 2 == 1:  
+                result *= x
+            x *= x  
+            n //= 2  
+        
+        return result
+        
