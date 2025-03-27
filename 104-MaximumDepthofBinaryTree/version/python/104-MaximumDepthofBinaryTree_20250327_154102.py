c6dabe7cf8b4f2fd7fde70c2ade57f875f35464a
Ritik Zambre <116812243+ritikZ18@users.noreply.github.com>
2025-03-27 15:41:00 -0700

    Create file

---
+++ 104-MaximumDepthofBinaryTree/version/python/104-MaximumDepthofBinaryTree_20250327_154102.py
@@ -0,0 +1,20 @@
+# Last updated: 3/27/2025, 3:41:02 PM
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        
+        # reccursion 
+        if not root : 
+            return 0 
+        
+        # node = 1 , depth 
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
